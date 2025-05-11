a2583231a7be2dc74da9e71e36b6a671058e11bd
s237129 <s237129@dtu.dk>
2025-05-11 22:00:06 +0100

    First draft for home (login) page No functionality yet

--- app/src/main/java/pt/ist/cmu/chargist/MainActivity.kt
+++ app/src/main/java/pt/ist/cmu/chargist/MainActivity.kt
@@ -1,17 +1,43 @@
 package pt.ist.cmu.chargist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import pt.ist.cmu.chargist.ui.theme.ChargISTTheme
+
+val appColor = Color.hsv(150f, 0.7f, 0.8f)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChargISTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomePage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +56,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomePage(modifier: Modifier = Modifier) {
+    var user by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column (
+        modifier = modifier.fillMaxHeight().fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to ChargIST",
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+        OutlinedTextField(
+            value = user,
+            onValueChange = {user = it},
+            label = {Text("Username")},
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = {Text("Password")},
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        OutlinedButton(
+            onClick = {},
+            colors = ButtonColors(Color.Transparent, appColor, Color.Transparent, Color.LightGray),
+            shape = RoundedCornerShape(6.dp),
+            border = BorderStroke(2.dp, appColor)
+        ) { Text("Login") }
+        Spacer(Modifier.size(5.dp))
+        TextButton(
+            onClick = {Log.d("HomePage","User: $user and password: $password")}, // log for user credentials
+        ) { Text("Create Account") }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomePagePreview() {
     ChargISTTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
